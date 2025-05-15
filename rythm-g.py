550cc568c3273c7aa1896277c260a13163995861
ShyamRV <shyamjipandey211105@gmail.com>
2025-05-15 13:03:46 +0530

    Text file has been upated to python file.

--- rythm-g.py
+++ rythm-g.py
@@ -1,1 +1,3 @@
-"File has been updated from text file to python file for smoother operations, eassy access and user convinience."
+"File has been updated from text file to python file for smoother operations, eassy access and 
+user convinience."
+print("Hello World!")
